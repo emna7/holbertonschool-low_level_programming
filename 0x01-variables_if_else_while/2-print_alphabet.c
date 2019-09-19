8014189a7ef1d1a21fa7ae55ec40441455a3e459
emna7 <bhmemna7@gmail.com>
2019-09-19 14:51:08 +0000

    all my work is back

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print_alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+for(c = 'a'; c <= 'z'; ++c)
+putchar(c);
+putchar('\n');
+return 0;
+}
+
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print_alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+char cc;
+for (c = 'a'; c <) 'z'; c++)
+putchar(c);
+for (cc = 'A'; cc <= 'Z'; cc++)
+putchar(cc);
+putchar('\n');
+return (0);
+}
