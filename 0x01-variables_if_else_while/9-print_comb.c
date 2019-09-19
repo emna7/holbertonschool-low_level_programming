6b9c705dfdb5bb690f2a70e87090359d872f5674
Emna <bhmemna7@gmail.com>
2019-09-19 21:31:11 +0000

    create

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * print_single digit numbers
+ * return 0 (success)
+*/
+int main(void)
+int i;
+for (i = 48 ; i <= 57; i++)
+{
+putchar(i);
+if ( i < 57 )
+{
+putchar(44);
+putchar(32);
+}
+return(0);
