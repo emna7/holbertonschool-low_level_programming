23169d944afe2f94dad44098de84dd52ca11af6d
Emna <bhmemna7@gmail.com>
2019-09-19 20:38:08 +0000

    change

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
-
+/**
+ * main - Entry point
+ * print_alphabet
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char c;
-for (c= 'z'; c >= 'a' ; c--)
+for (c = 'z'; c >= 'a' ; c--)
 putchar(c);
-putchar(10);
+putchar('\n');
 return(0);
 }
