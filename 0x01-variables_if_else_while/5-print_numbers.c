e38b7bba35bbfb98b890ef7d1d805c7dab275aa0
Emna <bhmemna7@gmail.com>
2019-09-19 19:48:09 +0000

    create

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * print_alphabet
+ * Return: always 0 (success)
+*/
+void main()
+{
+int i;
+for (i = 0; i < 10; i++)
+printf("%i", i);
+printf("\n");
+return (0);
+}
