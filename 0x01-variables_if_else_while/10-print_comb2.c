399f594923a0849c466453285f3d4b5dc57e06c5
Emna <bhmemna7@gmail.com>
2019-09-19 21:58:32 +0000

    create

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * print_from 00 to 99
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+int j;
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+		putchar(i);
+		putchar(j);
+		putchar(',');
+		putchar(' ');
+		}
+	}
+putchar('\n');
+return (0);
+}
