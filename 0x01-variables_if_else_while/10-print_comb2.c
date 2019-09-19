6e8d3d26c8799877831ce3f739a7e8ef3c500b32
Emna <bhmemna7@gmail.com>
2019-09-19 22:01:14 +0000

    change

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -8,16 +8,16 @@ int main(void)
 {
 int i;
 int j;
-	for (i = 48; i <= 57; i++)
-	{
-		for (j = 48; j <= 57; j++)
-		{
-		putchar(i);
-		putchar(j);
-		putchar(',');
-		putchar(' ');
-		}
-	}
+for (i = 48; i <= 57; i++)
+{
+for (j = 48; j <= 57; j++)
+{
+putchar(i);
+putchar(j);
+putchar(',');
+putchar(' ');
+}
+}
 putchar('\n');
 return (0);
 }
