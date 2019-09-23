e3e288dbd9312856f926e63bb7b88c5bc3fa5e5a
Emna <bhmemna7@gmail.com>
2019-09-23 13:06:58 +0000

    create

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * print_alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+putchar ("Holberton");
+putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * print_alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+}
