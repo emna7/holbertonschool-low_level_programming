9ec0fafe695bc0abac0602ce430ffde19c68b452
Emna <bhmemna7@gmail.com>
2019-09-23 19:38:43 +0000

    create

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include"holberton.h"
+/**
+ * main - Entry point
+ * _Putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+print_alphabet_x10();
+return (0);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -7,7 +7,17 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
+}
+void print_alphabet_x10(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
