340fa07c037610b4cdb8a96e4f7d0bef185b35fb
Emna <bhmemna7@gmail.com>
2019-09-23 19:01:42 +0000

    change

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,9 +6,15 @@
  */
 int main(void)
 {
-int n;
-for (n == Holberton)
-_putchar(n);
+_putchar("H");
+_putchar("o");
+_putchar("l");
+_putchar("b");
+_putchar("e");
+_putchar("r");
+_putchar("t");
+_putchar("o");
+_putchar("n");
 _putchar('\n');
 return (0);
 }
