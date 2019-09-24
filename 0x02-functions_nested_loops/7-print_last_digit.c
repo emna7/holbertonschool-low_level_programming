3281a9a9cbd49168d8061f7d2030d9cb6d4c24fa
Emna <bhmemna7@gmail.com>
2019-09-24 10:44:21 +0000

    change

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,18 @@
-#include"holberton.h"
+#include "holberton.h"
+
 /**
- * print_last_digit - prints the last digit of an integer
+ * print_last_digit - prints the last digit of a number
  * @r: number to find last digit of
+ *
  * Return: last digit of number r
-*/
+ */
+
 int print_last_digit(int r)
 {
-int l = r % 10;
-{
-return (l);
-}
-}
+	int l = r % 10;
+
+	if (l >= 0)
+	{
+		_putchar(l + '0');
+		return (l);
+	}
