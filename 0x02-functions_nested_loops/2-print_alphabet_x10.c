b71c10131fc9d5b45f8622a9e320866b4734f7d6
Emna <bhmemna7@gmail.com>
2019-09-24 10:23:44 +0000

    change

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,12 @@
 #include"holberton.h"
 /**
- * main - Entry point
- * _Putchar
+ * print_alphabet - Entry point
+ * prints alphabet ten times
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet_x10(void)
 {
 int i;
 for (i = 0 ; i < 10 ; i++)
 print_alphabet_x10();
-return (0);
 }
