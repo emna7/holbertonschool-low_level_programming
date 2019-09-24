be5de0dc898c640ba98a2b861f6a646f9eecca64
Emna <bhmemna7@gmail.com>
2019-09-24 13:37:01 +0000

    change

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,12 +12,12 @@ for (h = 0; h < 24; h++)
 {
 for (m = 0; m < 60; m++)
 {
-_putchar(h / 10);
-_putchar(h % 10);
-_putchar(':');
-_putchar(m / 10);
-_putchar(m % 10);
-_putchar('\n');
+_putchar((h / 10)+ '0') ;
+_putchar((h % 10)+ '0') ;
+_putchar((':')+ '0') ;
+_putchar((m / 10)+ '0') ;
+_putchar((m % 10)+ '0') ;
+_putchar(('\n')+ '0') ;
 }
 }
 }
