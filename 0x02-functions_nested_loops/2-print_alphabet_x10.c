3b56f54d1dd5953238e938947aed2ca467d92bf9
Emna <bhmemna7@gmail.com>
2019-09-24 10:31:06 +0000

    change

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,11 @@
  */
 void print_alphabet_x10(void)
 {
-int i;
-for (i = 0 ; i < 10 ; i++)
-print_alphabet_x10();
+int i, j;
+for (j = 0; j < 10; j++)
+{
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
+}
 }
