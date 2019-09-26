0d104f2080ec81bc5c6e0fe2bd16d204f97e4132
Emna <bhmemna7@gmail.com>
2019-09-26 10:53:07 +0000

    create

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_line - function that prints a straight line
+ * @n: the number of times the character _ should be printed
+ * Return: 0
+*/
+void print_line(int n)
+{
+int a
+for (a = 0; a <= n; a++)
+{
+_putchar('_');
+_putchar('\n');
+}
+}
+
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,5 +1,7 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_numbers(int x);
-void print_most_numbers(int i);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
