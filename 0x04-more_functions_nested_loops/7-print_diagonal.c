98df2ac34b9a88149bf89d94e909b2074f850c6a
Emna <bhmemna7@gmail.com>
2019-09-26 14:39:06 +0000

    maj

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,15 +4,26 @@
  * @n: the number of times the character \ should be printed
  * Return 0
 */
-void print_diagonal(int n);
+void print_diagonal(int n)
 {
-int a
-if ( n > 0)
+int count, count_2;
+
+for (count = 0; count < n; count++)
 {
-for ( a = 0; a <= n; a++)
+for (count_2 = 0; count_2 <= count; count_2++)
 {
-_putchar("\");
+if (count_2 != count)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('\\');
+}
+
+}
 _putchar('\n');
 }
-}
+if (n <= 0)
+_putchar('\n');
 }
