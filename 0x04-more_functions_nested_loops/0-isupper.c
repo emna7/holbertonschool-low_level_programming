a252b7e6eb2612bb7d50c1b240f9b58a93d43cfd
Emna <bhmemna7@gmail.com>
2019-09-26 09:22:12 +0000

    create

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,19 @@
-
+#include "holberton.h"
+/**
+ * _isupper - checks is an alpha is uppercase
+ * @c: variable to check
+ * Return: 1 if uppercase, 0 otherwise
+*/
+int _isupper(int c)
+{
+char c;
+if (c >= 'A' && c <= 'Z')
+{
+_putchar("c is upper")
+return(1);
+}
+else
+{
+return(0);
+}
+}
