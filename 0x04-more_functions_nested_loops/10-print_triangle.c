9e87c714a1a5f0f5a00d61a9d73522e63fd4dee7
Emna <bhmemna7@gmail.com>
2019-09-26 19:04:22 +0000

    change

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,12 +6,13 @@
 */
 void print_triangle(int size)
 {
-int i, j, rows;
+int i, j;
 if (size <= 0)
 _putchar('\n');
+else
 for (i = 0; i < size; i++)
 {
-for (j = 0; j < i; ++j)
+for (j = 0; j < i; j++)
 {
 _putchar("#");
 }
