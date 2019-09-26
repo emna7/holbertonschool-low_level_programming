84bb771f9f8dbadbba5f233c685f457035849c4c
Emna <bhmemna7@gmail.com>
2019-09-26 20:03:14 +0000

    change

---
+++ 0x04-more_functions_nested_loops/100-prime-factor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - function that prints the prime factor of a number
+ *
+ * return: 0 Always
+*/
+int main(void)
+{
+long n; i;
+i = 2;
+n = 612852475143
+for (i = 2; i <= a; i++)
+{
+while (n % i == 0)
+printf("maxPrime = i");
+}
+return (0);
+}
