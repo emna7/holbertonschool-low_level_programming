cdb907771d1290780c87f84fa33b8c05464d3026
Emna <bhmemna7@gmail.com>
2019-09-26 14:08:45 +0000

    maj

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
 /**
  * more_numbers - prints number from 0 to 14
- * @i: variable
- * @j: variable
+ *
  * Return 0
 */
 void more_numbers(void)
 {
 int i, j;
-for (j = 0; j <= 10; j++)
+for (j = 0; j < 10; j++)
 {
 for (i = 0; i <= 14; i++)
 if (i >= 10)
