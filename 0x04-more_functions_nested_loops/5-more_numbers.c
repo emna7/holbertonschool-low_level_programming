7a924f79d9e8dcd03595e22c930ee4ec579979ac
Emna <bhmemna7@gmail.com>
2019-09-26 19:31:15 +0000

    change

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,11 +6,11 @@
 */
 void more_numbers(void)
 {
-int i, j;
+int i;
 for (j = 0; j < 10; j++)
+_putchar(i + '0');
+for (i = 10; i <= 14; i++)
 {
-for (i = 0; i <= 14; i++)
-if (i >= 10)
 _putchar((i / 10) + '0');
 _putchar((i % 10) + '0');
 _pucthar('\n');
