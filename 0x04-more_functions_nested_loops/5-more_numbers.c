375831c380763bb7d31dd7b0ceb1903702367f8b
Emna <bhmemna7@gmail.com>
2019-09-26 14:11:04 +0000

    maj

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,8 +11,8 @@ for (j = 0; j < 10; j++)
 {
 for (i = 0; i <= 14; i++)
 if (i >= 10)
-_putchar(i / 10 + '0');
-_putchar(i % 10 + '0');
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
 _pucthar('\n');
 }
 }
