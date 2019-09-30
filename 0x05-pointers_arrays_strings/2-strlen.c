334d3b407b6f6cc0aadee8637981dcf94802f29b
Emna <bhmemna7@gmail.com>
2019-09-30 18:10:41 +0000

    create

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include"holberton.h"
+/**
+ * strlen - function that returns the length of a string
+ * @s: pointer
+ * return: i
+*/
+int _strlen(char *s)
+{
+int i;
+i = 0;
+while ((*(s+i)) != '\0')
+i++;
+return (i);
+}
