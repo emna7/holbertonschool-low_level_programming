fb9719c1f07106e070829e98b10eefb72933f540
Emna <bhmemna7@gmail.com>
2019-10-01 19:23:31 +0000

    create

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,10 +7,14 @@
 void puts2(char *str)
 {
 int i;
-for (i = 0; *(str + i) <= '\0'; i++)
+i = 0;
+while (*(str + i) != '\0')
 {
 if (i % 2 == 0)
+{
 _putchar(*(str + i));
 _putchar('\n');
+}
+i++;
 }
 }
