972d14bd86ab3a6776b7fc6e3ddbe0da4fef7d95
Emna <bhmemna7@gmail.com>
2019-10-01 09:23:20 +0000

    create

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,17 @@
 #include"holberton.h"
 /**
  * _puts(char *str) - function that prints a string followed by a new line
- * @*str: pointer
+ * @str: pointer
  * Return: 0
 */
 void _puts(char *str);
 {
 int i;
 i = 0;
-while ((*(str + i)) != '\0')
+while (*(str + i) != '\0')
 {
 _putchar(*(str + i));
 i++;
 }
 _putchar('\n');
-return (0);
 }
