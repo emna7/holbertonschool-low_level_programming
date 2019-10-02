53deb3cddd7afbf510ffb5bf1fb791d59fa23629
Emna <bhmemna7@gmail.com>
2019-10-02 13:49:51 +0000

    change

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,20 +8,21 @@ int _atoi(char *s)
 {
 int i, j, signe, val;
 i = 0; j = 1;
-while(*(s + i) != '\0')
+while(s[i] != '\0')
+i++;
 {
-if (*(s + i) == '+')
+if (s[i] == '+')
 {
 signe = signe * 1;
 }
-else if (*(s + i) == '-')
+else if (s[i] == '-')
 {
 signe = signe * -1;
 }
-else if ((*(s + i) >= '0' && *(s + i) <= '9'))
+else if ((s[i] >= '0' && s[i] <= '9'))
 {
-val = (j * val) + *(s + i);
-j = j * 10 + (*(s + i) - '0');
+val = (j * val) + s[i];
+j = j * 10 + (s[i] - '0');
 }
 }
 val = val * signe;
