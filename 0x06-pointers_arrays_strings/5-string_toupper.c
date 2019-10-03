96fcaa19d77cf851b9ceb0decb34fa0be30cf5f6
Emna <bhmemna7@gmail.com>
2019-10-03 14:03:07 +0000

    change

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include"holberton.h"
+/**
+ * string_toupper - change all lowercase to upper
+ * @s: pointer to array
+ * Return: character
+*/
+char *string_toupper(char *)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (s[i] = 97; s[i] <= 122 || s[i] = 65; s[i] <= 90)
+(s[i] = 65 && s[i] <= 90);
+i++;
+}
+return (s);
+}
