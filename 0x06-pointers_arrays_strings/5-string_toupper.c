98e206d41fb4e72bef6346910c4316a4bc651dad
Emna <bhmemna7@gmail.com>
2019-10-03 15:38:11 +0000

    create

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,14 +6,9 @@
 */
 char *string_toupper(char *s)
 {
-int i;
-while (s[i] != '\0')
+if (*s == 97 && *s <= 122)
 {
-if (s[i] == 97 && s[i] <= 122)
-{
-s[i] = 90 - (122 - s[i]);
+*s = 90 - (122 - *s);
 }
-i++;
-}
-return (s);
+return (*s);
 }
