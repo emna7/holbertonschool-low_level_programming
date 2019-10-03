7d6e5768d4bef262acd7eb7880a16316bd650e68
Emna <bhmemna7@gmail.com>
2019-10-03 18:52:13 +0000

    create

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,14 @@ int _strcmp(char *s1, char *s2)
 {
 int i, x;
 i = 0;
+if (*s1 == *s2){
+return (0);
+};
 while (s1[i] != '\0' || s2[i] != '\0')
 {
 if (s1[i] != s2[i])
 {
-x += s1[i] - s2[i];
+x += *s1[i] - *s2[i];
 }
 i++;
 }
