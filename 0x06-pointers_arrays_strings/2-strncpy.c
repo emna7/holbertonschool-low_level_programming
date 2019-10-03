75bfe533ba0ef2afc7b57b540661ddbac0140cf9
Emna <bhmemna7@gmail.com>
2019-10-03 12:28:53 +0000

    change

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,10 +8,17 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i, j;
+int i;
 i = 0;
-for (i = 0; src[i] != '\0' && i < n; i++)
-j = 0;
-*(dest + j) = *(src + i);
+while (src[i] != '\0' && i < n)
+{
+*(dest + i) = *(src + i);
+i++;
+}
+while (i < n)
+{
+*(dest + i) = '\0';
+i++;
+}
 return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include"holberton.h"
+/**
+ * _strcmp - function that compares two strings
+ * @s1: pointer
+ * @s2: pointer
+ * Return: 0
+*/
+int _strcmp(char *s1, char *s2)
+{
+int i, j, x;
+i = 0;
+j = 0;
+while (s1[i] != '\0')
+i++;
+while (s2[j] != '\0')
+j++;
+for (i < j || i = j || i > j)
+_putchar(x = i - j + '0');
+return (x);
+}
