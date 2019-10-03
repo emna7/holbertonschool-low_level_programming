9dda0967b1c98f33b1bbfe8083b09e104825fb02
Emna <bhmemna7@gmail.com>
2019-10-03 18:18:28 +0000

    create

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,22 +1,30 @@
-#include"holberton.h"
+#include "holberton.h"
 /**
- * rot13 - function that encodes a string using rot13
- * @s: pointer
- * Return: s
+* rot13 - rot13 enoding generator *
+* @s: char pointer *
+* Return: return char *
 */
 char *rot13(char *s)
 {
-int i = 0;
+int i;
+int j;
+int n = 0;
+char oldalpha[] = "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz";
+char newalpha[] = "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm";
+i = 0;
+j = 0;
 while (s[i] != '\0')
 {
-if ((s[i] - 65) > 12 || (s[i] - 97) > 12)
+while (oldalpha[j] != '\0' && n == 0)
 {
-s[i] = s[i] - 13;
-}
-else
+if (s[i] == oldalpha[j])
 {
-s[i] = s[i] + 13;
+s[i] = newalpha[j];
+n = 1;
 }
+j++;
+}
+n = 0;
 i++;
 }
 return (s);
