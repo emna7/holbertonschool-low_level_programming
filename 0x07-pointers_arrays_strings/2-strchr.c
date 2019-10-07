0d6d439c713edf02badf3e6e031f29800722684f
Emna <bhmemna7@gmail.com>
2019-10-07 12:12:24 +0000

    create

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include"holberton.h"
+/**
+ * _strchr - finds if char occurs in string
+ * @s: string to check
+ * @c: character to find in s
+ * Return: pointer to first instance of c, otherwise NULL
+*/
+char *_strchr(char *s, char c)
+{
+int i = 0;
+while (*(s + i) != '\0')
+{
+if (s[i] == c)
+return (s);
+i++;
+if (*(s + i) != c)
+return (0);
+}
+return (s);
+}
