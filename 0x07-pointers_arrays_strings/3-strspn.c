7ac1fc5829b85532767312c202340a9a324f5efa
Emna <bhmemna7@gmail.com>
2019-10-07 18:01:15 +0000

    create

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,20 +9,23 @@ unsigned int _strspn(char *s, char *accept)
 {
 unsigned int n = 0;
 int i = 0;
-int j = 0;
 while (*(s + i) !=  '\0')
 {
+int j = 0;
 while (*(accept + j) != '\0')
 {
-if (s[0] != accept[0])
-return (n);
-else if (s[i] == accept[j])
+if (s[i] == accept[j])
 {
 n++;
+break;
+}
+if (accept[j] == '\0')
+{
+return (n);
 }
 j++;
 }
-i++
+i++;
 }
 return (n);
 }
