878c7d8a0ec51343918b4e79e7aaf072b0631638
Emna <bhmemna7@gmail.com>
2019-10-07 18:20:34 +0000

    create

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,4 @@
 #include"holberton.h"
-#include<stdlib.h>
 /**
  * _strstr - searches a string for a a substring
  * @needle: what we're searching for
@@ -14,15 +13,15 @@ i = 0;
 while (haystack[i] != '\0')
 {
 j = 0;
-while (needle[j + x] != '\0' && haistack[i + x] != '\0'
-&& needle[j + x] == haistack[i + x])
+while (needle[j + x] != '\0' && haystack[i + x] != '\0'
+&& needle[j + x] == haystack[i + x])
 {
-if (haistack[i + x] != needle[j + x])
+if (haystack[i + x] != needle[j + x])
 break;
 x++;
 }
 if (needle[j + x] == '\0')
-return (&haistack[i]);
+return (&haystack[i]);
 j++;
 i++;
 }
