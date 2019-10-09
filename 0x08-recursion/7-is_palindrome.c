739f4c702ba6d0a7f819226f10d3f9fdbaf74f3c
Emna <bhmemna7@gmail.com>
2019-10-09 18:51:46 +0000

    create

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strlen_recursion - Finds the length of the string to be tested
+ * @s: The string to test
+ * Return: The length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+else
+return (_strlen_recursion(s + 1) + 1);
+}
+
+/**
+ * palindrome_checker - function for recursive palindrome algo
+ * @s: String to check
+ * @start: First char of string to check
+ * @end: Last char of string to check
+ *
+ * Return: 1 if str is palindrome, 0 otherwise
+ */
+
+int palindrome_checker(char *s, int start, int end)
+{
+if (s[start] != s[end])
+return (0);
+if (start >= end)
+return (1);
+return (palindrome_checker(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - Tests whether a string is a palindrome
+ * @s: The string to test
+ * Return: 1 if palindrome, 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+int end = _strlen_recursion(s);
+if (!palindrome_checker(s, 0, end - 1))
+return (0);
+return (1);
+}
