e24575039060dc885016df11c10f58c949dd4370
Emna <bhmemna7@gmail.com>
2019-10-09 15:05:18 +0000

    create

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include"holberton.h"
+/**
+ * _pow_recursion - pow function
+ * @y: int
+ * @x: int
+ * Return: success
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+else if (y == 0)
+return (1);
+else
+return (x * (_pow_recursion(x, y - 1)));
+}
