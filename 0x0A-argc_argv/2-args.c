a2f445ed8d205b630ee923743031db05e3cdd0da
Emna <bhmemna7@gmail.com>
2019-10-13 12:47:50 +0000

    change

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,10 +5,10 @@
  * @argv: argument vector
  * Return: 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 int i;
-for(i = 0; i < argc; i++)
+for (i = 0; i < argc; i++)
 printf("%s\n", argv[i]);
 return (0);
 }
