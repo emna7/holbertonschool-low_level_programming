1103f514f8ee2dc0f1fb72bc703963cde7b13b99
Emna <bhmemna7@gmail.com>
2019-10-13 12:16:18 +0000

    create

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - prints the number of arguments passed into it
- *@argc: number of arguments
- *@argv: argument vector
- *Return: 0
+ * @argc: number of arguments
+ * @argv: argument vector
+ * Return: 0
  */
-int main (int argc, char *argv[] __attribute__((unused)))
+int main (int argc, char const *argv[] __attribute__((unused)))
 {
 printf("%d\n", argc - 1);
 return (0);
