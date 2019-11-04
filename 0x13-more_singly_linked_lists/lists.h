32bf19af560e528e5527e093fff669e159a246e2
Emna <bhmemna7@gmail.com>
2019-11-04 12:59:16 +0000

    start

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,18 @@
+#ifndef _LISTSHEADER_
+#define _LISTSHEADER_
+#include <stdio.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+#endif
