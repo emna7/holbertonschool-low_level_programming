0d46f13ad319b10a3ebdff650fa6760c3f0df881
Emna <bhmemna7@gmail.com>
2019-11-06 10:06:14 +0000

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,14 +8,15 @@
 */
 void free_listint2(listint_t **head)
 {
-listint_t *temp = *head;
+listint_t *temp, *new;
 if (*head == NULL)
 return;
-while (*head != NULL)
+new = *head;
+while (new != NULL)
 {
-temp = (*head)->next;
-free(*head);
-*head = temp;
+temp = new;
+new = new->next;
+free(temp);
 }
 *head = NULL;
 }
