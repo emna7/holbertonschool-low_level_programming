0c36c0b137d7ba9006edcb92d210ce31c53e0222
Emna <bhmemna7@gmail.com>
2019-11-07 17:35:13 +0000

    create

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,8 +12,9 @@ int k, i;
 if (n < 2)
 {
 _putchar(n + '0');
-return;
 }
+else
+{
 for (i = 31; i >= 0; i--)
 {
 k = n >> i;
@@ -23,6 +24,7 @@ if (k & 1)
 _putchar('1');
 else
 _putchar('0');
+}
 }
 }
 }
