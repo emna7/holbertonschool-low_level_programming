b96e0b5c7202b217e6f095159a2a7e5459f09608
Emna <bhmemna7@gmail.com>
2019-11-07 17:49:28 +0000

    task2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * get-bit - function that returns the value of a bit
+ * @n: unsigned long int
+ * @index: the given index
+ * Return: the value of the bit
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+int val;
+if (index > 31)
+return (-1);
+val = (n >> index) & 1;
+return (val);
+}
