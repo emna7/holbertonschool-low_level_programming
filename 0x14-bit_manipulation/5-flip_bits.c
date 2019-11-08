8ea55b0c11c62d1d7e9191400c9568f2276d9084
Emna <bhmemna7@gmail.com>
2019-11-08 12:32:42 +0000

    change

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,7 +9,7 @@
 */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-unsigned int bits = 0, xor;
+unsigned long int bits = 0, xor;
 for (xor = n ^ m; xor != 0; xor = xor >> 1)
 bits += xor & 1;
 return (bits);
