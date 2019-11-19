f9756eeaf1cc9400c9e372624a9af26d130fbb91
Emna <bhmemna7@gmail.com>
2019-11-19 23:52:31 +0000

    create

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,31 @@
-#include"holberton.h"
+#include "holberton.h"
 /**
- * cap_string - function that capitalizes a char
- * @s: pointer
- * Return: pointer to a string s */
-char *cap_string(char *s) {
-int i, j;
-char sep[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-for (i = 0; s[i] != '\0'; i++)
+ * cap_string - capitalizes all words in a string
+ * @s: string to capitalize
+ *
+ * Return: address of s
+ */
+char *cap_string(char *s)
 {
-for (j = 0; sep[j] != '\0'; j++)
+int i = 0, j;
+char a[] = " \t\n,;.!?\"(){}";
+
+while (*(s + i))
+{
+if (*(s + i) >= 'a' && *(s + i) <= 'z')
 {
 if (i == 0)
+*(s + i) -= 'a' - 'A';
+else
 {
-if (s[i] >= 'a' && s[i] <= 'z')
+for (j = 0; j <= 12; j++)
 {
-s[i] = s[i] - 32;
-}
-}
-if (s[i] == sep[j])
-{
-if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-{
-s[i + 1] = s[i + 1] - 32;
+if (a[j] == *(s + i - 1))
+*(s + i) -= 'a' - 'A';
 }
 }
 }
+i++;
 }
 return (s);
 }
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,30 +1,22 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
 /**
- * print_binary - function that converts numbers to binary
- * @n: number to convert
- * Return: nothing
+* print_binary - prints the binary rep
+* of a number.
+* @n: unsigned long int.
+* Return: nothing
 */
 void print_binary(unsigned long int n)
 {
-int k, i;
-if (n < 2)
+if (n >> 0)
 {
-_putchar(n + '0');
+if (n >> 1)
+print_binary(n >> 1);
+_putchar((n & 1) + '0');
 }
 else
 {
-for (i = 31; i >= 0; i--)
-{
-k = n >> i;
-if (k != 0)
-{
-if (k & 1)
-_putchar('1');
-else
 _putchar('0');
-}
-}
 }
 }
