78960cd9d8cf6c24c24fc8c00c4f7c4da3c547f3
Emna <bhmemna7@gmail.com>
2019-11-20 00:14:29 +0000

    create

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,59 +1,68 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- * Return: Always 0.
-*/
-int main(int argc, char **argv)
+ * errorHandle - handles errors.
+ * @src: source fd.
+ * @dest: dest fd.
+ * @close: close status.
+ * @args: arguments.
+ * Return: status.
+ */
+void errorHandle(int src, int dest, int close, char *args[])
 {
-int x1, x2, rd, wr;
-char buf[1024];
+if (src < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", args[1]);
+exit(98);
+}
+if (dest < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", args[2]);
+exit(99);
+}
+if (close < 0 && src > 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", args[1]);
+exit(100);
+}
+if (close < 0 && dest > 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", args[2]);
+exit(100);
+}
+}
+/**
+ * main - copies file.
+ * @argc: num of args.
+ * @argv: arguments.
+ * Return: status.
+ */
+int main(int argc, char *argv[])
+{
+int filefrom, fileto;
+int r, w, c;
+char buffer[1024];
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-if (argv[1] == NULL)
+filefrom = open(argv[1], O_RDONLY);
+errorHandle(filefrom, 1, 1, argv);
+fileto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+errorHandle(1, fileto, 1, argv);
+r = 1024;
+while (r == 1024)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+r = read(filefrom, buffer, 1024);
+errorHandle(r, 1, 1, argv);
+w = write(fileto, buffer, r);
+if (w != r)
+w = -1;
+errorHandle(1, w, 1, argv);
 }
-x1 = open(argv[1], O_RDWR, 0777);
-if (x1 == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
-x2 = open(argv[2],  O_RDWR | O_CREAT | O_TRUNC, 0664);
-if (x2 == -1)
-{
-dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
-exit(99);
-}
-rd = read(x1, buf, 1024);
-while (rd > 0)
-{
-wr = write(x2, buf, rd);
-if (wr != rd)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-exit(99);
-}
-rd = read(x1, buf, 1024);
-}
-if (rd == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
-if (close (x1) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[1]);
-exit(100);
-}
-if (close(x2) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[2]);
-exit(100);
-}
+c = close(filefrom);
+errorHandle(filefrom, 1, c, argv);
+c = close(fileto);
+errorHandle(1, fileto, c, argv);
 return (0);
 }
