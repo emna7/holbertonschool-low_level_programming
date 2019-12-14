bfe7dedf90f74a96a7447fec86a45e64b11dc957
Emna <bhmemna7@gmail.com>
2019-12-14 11:40:49 +0000

    create

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,23 @@
 #ifndef _LISTSHEADER_
 #define _LISTSHEADER_
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
+typedef struct dlistint_s
+{
+int n;
+struct dlistint_s *prev;
+struct dlistint_s *next;
+} dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
 
