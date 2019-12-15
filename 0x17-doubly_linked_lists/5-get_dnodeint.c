0764578750db0935c7d62fcb6e201348497e0af3
Emna <bhmemna7@gmail.com>
2019-12-15 17:52:03 +0000

    create

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -7,16 +7,10 @@
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-unsigned int i = 0;
-if (head == NULL)
-return (NULL);
-while (head != NULL && i < index)
-{
-head = head->next;
-i++;
-}
-if (i == index)
-return (head);
-else
-return (NULL);
+dlistint_t *y;
+unsigned int c;
+y = head;
+for (c = 0; y && c < index; y = y->next, c++)
+;
+return (y);
 }
